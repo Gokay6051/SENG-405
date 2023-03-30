108938bf52dc75156477cd3cd10fd1e4592dbb7b
Ahmet Gökay Ürkmez <gokayurkmez_541@hotmail.com>
2023-03-30 22:51:58 +0300

    Project Completed

---
+++ TimeGame/app/src/main/java/com/example/timegame/GameActivity.java
@@ -0,0 +1,102 @@
+package com.example.timegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class GameActivity extends AppCompatActivity {
+
+    private Button p1Button, p2Button;
+    private TextView textTimer;
+    //remaining time
+    private long remanTime;
+    //variables from MainActivity
+    private String P1, P2;
+    //flag to control buttons enable and disable
+    private int flag;
+    CountDownTimer mCountDownTimer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        remanTime = 16000;
+        p1Button = (Button)findViewById(R.id.player1Button);
+        p2Button = (Button)findViewById(R.id.player2Button);
+        textTimer = (TextView)findViewById(R.id.timerText);
+
+        flag = 0;
+
+        Intent incomingIntent = getIntent();
+        P1 = incomingIntent.getStringExtra("player1");
+        P2 = incomingIntent.getStringExtra("player2");
+
+        p1Button.setText(P1);
+        p2Button.setText(P2);
+    }
+
+
+    //is flag 0 -> start timer and set flag variable to 1,     is flag 1-> stop timer and set flag variable to 0 and disable p1Button, enable p2Button
+    public void startButton1(View v) {
+        if (flag==0) {
+            //will be functionalized and optimized
+            mCountDownTimer = new CountDownTimer(remanTime, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    // Update the UI with the remaining time
+                    textTimer.setText("Time remaining: " + millisUntilFinished / 1000);
+                    remanTime = millisUntilFinished;
+                }
+
+                @Override
+                public void onFinish() {
+                    // Handle the timer finish event
+                    textTimer.setText(P2 + " is won");
+                    remanTime = 16000;
+                }
+            }.start();
+            flag = 1;
+        }
+        else if(flag==1) {
+            mCountDownTimer.cancel();
+            flag = 0;
+            p1Button.setEnabled(false);
+            p2Button.setEnabled(true);
+        }
+    }
+
+    //is flag 0 -> start timer and set flag variable to 1,     is flag 1-> stop timer and set flag variable to 0 and disable p2Button, enable p1Button
+    public void startButton2(View v) {
+        if (flag==0) {
+            //will be functionalized and optimized
+            mCountDownTimer = new CountDownTimer(remanTime, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    // Update the UI with the remaining time
+                    textTimer.setText("Time remaining: " + millisUntilFinished / 1000);
+                    remanTime = millisUntilFinished;
+                }
+
+                @Override
+                public void onFinish() {
+                    // Handle the timer finish event
+                    textTimer.setText(P1 + " is won");
+                    remanTime = 16000;
+                }
+            }.start().start();
+            flag = 1;
+        }
+        else if(flag==1) {
+            mCountDownTimer.cancel();
+            flag = 0;
+            p2Button.setEnabled(false);
+            p1Button.setEnabled(true);
+        }
+    }
+}
--- TimeGame/app/src/main/java/com/example/timegame/MainActivity.java
+++ TimeGame/app/src/main/java/com/example/timegame/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.timegame;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.text.TextUtils;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -17,60 +19,38 @@ import com.example.timegame.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    private EditText player1, player2;
+    private TextView txtIsEmpty;
+    //player1 = p1, player2 = p2
+    private String p1, p2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        player1 = (EditText)findViewById(R.id.editTextPlayerName);
+        player2 = (EditText)findViewById(R.id.editTextPlayerName2);
+        txtIsEmpty = (TextView)findViewById(R.id.textViewError);
+    }
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+    public void buttonStart(View v){
+        p1 = player1.getText().toString();
+        p2 = player2.getText().toString();
 
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        if(!TextUtils.isEmpty(p1)){
+            if(!TextUtils.isEmpty(p2)){
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                intent.putExtra("player1", p1);
+                intent.putExtra("player2", p2);
+                startActivity(intent);
             }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
-
-        return super.onOptionsItemSelected(item);
+        txtIsEmpty.setText("Spaces cannot be left blank!");
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
